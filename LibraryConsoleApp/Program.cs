0218864945baa9e14b959de538f7e18f3093e4d3
Michael Thrane <michael.thrane@siemensgamesa.com>
2024-02-23 13:03:20 +0100

    Add project files.

---
+++ LibraryConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using LibrarySystem;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Book[] books = new Book[3];
+
+        books[0] = new Book("Harry Potter & The Goblet of Fire", "J.K. Rowling", "2000", "074754624X");
+        books[1] = new Book("Harry Potter & Books of Secrets", "J.K. Rowling", "1998", "074754534X");
+        books[2] = new Book("Harry Potter & Half-Blood Price", "J.K. Rowling", "1997", "0747543424X");
+
+       foreach (Book book in books)
+        {
+            string bookInfo = book.DisplayBookInfo();
+            Console.WriteLine(bookInfo);
+        }
+
+
+    }
+}
---
+++ LibrarySystem/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace LibrarySystem
+{
+    public class Book(string title, string author, string publicationYear, string isbn)
+    {
+        public string Title { get; set; } = title;
+        public string Author { get; set; } = author;
+        public string PublicationYear { get; set; } = publicationYear;
+        public string ISBN { get; set; } = isbn;
+
+        public string DisplayBookInfo()
+        {
+            return $"Title: {Title}, Author: {Author}, Publication Year: {PublicationYear}, ISBN: {ISBN}";
+        }
+    }
+}
